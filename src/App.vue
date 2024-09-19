627a518e50fa1f4acc9779de4340e9c6f3247ff5
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-19 09:49:33 +0430

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import Navbar from './components/Navbar.vue';
+  import HeroVue from './components/Hero.vue';
+  import HomeCards from './components/HomeCards.vue';
+</script>
+
+<template>
+  <div>
+    <Navbar/>
+    <HeroVue />
+    <HomeCards />
+  </div>
+</template>
---
+++ src/components/Card.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+        <slot></slot>
+    </div>
+</template>
